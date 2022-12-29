89966a1d48d90448ab6ce08cb004ed9ac391e68d
keumbong <87923413+keumbong@users.noreply.github.com>
2022-12-29 13:57:02 +0900

    Update 피자 나눠 먹기(3).cs

--- 피자 나눠 먹기(3).cs
+++ 피자 나눠 먹기(3).cs
@@ -1,33 +1,26 @@
 문제 설명
-정수가 담긴 리스트 num_list가 주어질 때, num_list의 원소 중 짝수와 홀수의 개수를 담은 배열을 return 하도록 solution 함수를 완성해보세요.
+머쓱이네 피자가게는 피자를 두 조각에서 열 조각까지 원하는 조각 수로 잘라줍니다. 피자 조각 수 slice와 피자를 먹는 사람의 수 n이 매개변수로 주어질 때, n명의 사람이 최소 한 조각 이상 피자를 먹으려면 최소 몇 판의 피자를 시켜야 하는지를 return 하도록 solution 함수를 완성해보세요.
 
 제한사항
-1 ≤ num_list의 길이 ≤ 100
-0 ≤ num_list의 원소 ≤ 1,000
+2 ≤ slice ≤ 10
+1 ≤ n ≤ 100
 
 입출력 예
-num_list	result
-[1, 2, 3, 4, 5]	[2, 3]
-[1, 3, 5, 7]	[0, 4]
+slice	n	result
+7	10	2
+4	12	3
 
 입출력 예 설명
 입출력 예 #1
 
-[1, 2, 3, 4, 5]에는 짝수가 2, 4로 두 개, 홀수가 1, 3, 5로 세 개 있습니다.
+10명이 7조각으로 자른 피자를 한 조각 이상씩 먹으려면 최소 2판을 시켜야 합니다.
 
 입출력 예 #2
 
-[1, 3, 5, 7]에는 짝수가 없고 홀수가 네 개 있습니다.
+12명이 4조각으로 자른 피자를 한 조각 이상씩 먹으려면 최소 3판을 시켜야 합니다.
 
 
-def solution(num_list):
-    even = 0
-    odd = 0
+def solution(slice, n):
+    answer = divmod(n, slice)
     
-    for i in num_list:
-        if i % 2 == 0:
-            even += 1
-        else:
-            odd += 1
-    
-    return [even, odd]
+    return answer[0]+1 if answer[1] != 0 else answer[0]
